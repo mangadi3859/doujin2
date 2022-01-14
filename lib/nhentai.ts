eb4bfe6633dad3f1a3181c2bc28278196cab64ef
mangadi3859 <wkomangadi44@gmail.com>
2022-01-14 11:11:38 +0800

    Dev Log #4

--- lib/nhentai.ts
+++ lib/nhentai.ts
@@ -1,5 +1,185 @@
-import axios from "axios";
+import { AxiosResponse, default as axios } from "axios";
+import Pdf from "pdfjs";
+import jimp from "jimp";
+// import superAgent from "superagent";
+// import agentProxy from "superagent-proxy";
+import { Readable } from "stream";
 
-export async function searchById() {
-    //TODO
+// agentProxy(superAgent);
+
+const INFORMATION_FILE = `This is Doujin Downloader made by I S L A\n\nYou Can also use my web to download doujin too...\n\n(https://isla-doujin.herokuapp.com)`;
+
+/**
+ * https://nhentai.net/api/gallery/${id}
+ * https://i.nhentai.net/galleries/${res.data.media_id}/${i + 1}.jpg
+ * https://i2.nhentai.net/galleries/${res.data.media_id}/${i + 1}.png
+ */
+
+interface IDoujinTitle {
+    english: string;
+    japanese: string;
+    pretty: string;
+}
+
+type PageExt = "p" | "j";
+
+interface IPage {
+    t: PageExt;
+    w: number;
+    h: number;
+}
+
+interface IImages {
+    pages: IPage[];
+    cover: IPage;
+    thumbnail: IPage;
+}
+
+type TagType = "parody" | "category" | "language" | "tag" | "character" | "group" | "artist";
+
+interface ITag {
+    d: number;
+    type: TagType;
+    name: string;
+    url: string;
+    count: number;
+}
+
+export interface INhentaiResponse {
+    id: number;
+    media_id: string;
+    title: IDoujinTitle;
+    images: IImages;
+    scanlator?: string;
+    upload_date: number;
+    tags: ITag[];
+    num_pages: number;
+    num_favorites: number;
+}
+
+class NhentaiResponse {
+    id: number;
+    media_id: string;
+    title: IDoujinTitle;
+    images: IImages;
+    scanlator?: string;
+    upload_date: number;
+    tags: ITag[];
+    parodies: ITag[];
+    characters: ITag[];
+    artists: ITag[];
+    groups: ITag[];
+    languages: ITag[];
+    categories: ITag[];
+    num_pages: number;
+    num_favorites: number;
+
+    constructor(private readonly _nhentai: INhentaiResponse) {
+        this.id = _nhentai.id;
+        this.media_id = _nhentai.media_id;
+        this.title = _nhentai.title;
+        this.images = _nhentai.images;
+        this.scanlator = _nhentai.scanlator;
+        this.upload_date = _nhentai.upload_date;
+        this.tags = _nhentai.tags.filter((f) => f.type === "tag");
+        this.parodies = _nhentai.tags.filter((f) => f.type === "parody");
+        this.characters = _nhentai.tags.filter((f) => f.type === "character");
+        this.artists = _nhentai.tags.filter((f) => f.type === "artist");
+        this.groups = _nhentai.tags.filter((f) => f.type === "group");
+        this.languages = _nhentai.tags.filter((f) => f.type === "language");
+        this.categories = _nhentai.tags.filter((f) => f.type === "category");
+        this.num_pages = _nhentai.num_pages;
+        this.num_favorites = _nhentai.num_favorites;
+    }
+}
+
+/** Search Doujin from nhentai.net
+ *
+ * @param id
+ * @returns
+ *
+ */
+export async function search(id: string): Promise<NhentaiResponse> {
+    if (!id) throw new TypeError("id is required");
+    const regex = new RegExp(/^((?:https?:\/\/|www\.)?nhentai\.net\/g\/)?(\d+)/, "i");
+    const code = id.match(regex);
+
+    if (!code) throw new TypeError("Invalid id");
+    id = id.replace(regex, "$2");
+    // https://nhentai.net/api/gallery/${id}/
+    // let res = <any>await superAgent.get(`https://nhentai.net/api/gallery/${id}`).catch(console.error);
+
+    let res = <AxiosResponse<INhentaiResponse>>await axios.get(`https://nhentai.net/api/gallery/${id}`).catch((e) => console.log(e.toString()));
+
+    return res?.data ? new NhentaiResponse(res.data) : null;
+    // return res?.body ? new NhentaiResponse(res.body) : null;
+}
+
+/**Turn nhentai into zip
+ *
+ * @param {string} id - nhentai id
+ */
+export async function download(id: string, destination: Readable): Promise<void> {
+    if (!id) throw new TypeError("id is required");
+    id = id.toString();
+    // const zip = new JsZip();
+    const regex = new RegExp(/^((?:https?:\/\/|www\.)?nhentai\.net\/g\/)?(\d+)/, "i");
+    const code = id.match(regex);
+
+    if (!code) throw new TypeError("Invalid id");
+    id = id.replace(regex, "$2");
+    let res = (await axios.get<INhentaiResponse>("https://nhentai.net/api/gallery/" + id)).data;
+
+    if (!res || !res.images) throw new Error("Doujin not found");
+    const folder = `[ISLA-DOUJIN] ${res.title.pretty} (${res.id})`;
+    // zip.folder(folder);
+
+    let promises = res.images.pages.map(async (obj, i) => {
+        switch (obj.t) {
+            case "j": {
+                var buf = await axios.get(`https://i.nhentai.net/galleries/${res.media_id}/${i + 1}.jpg`, { responseType: "arraybuffer", timeout: 5000 }).catch((err) => {
+                    console.log(err.message);
+                    return null;
+                });
+                break;
+            }
+
+            case "p": {
+                let img = await jimp.read(`https://i2.nhentai.net/galleries/${res.media_id}/${i + 1}.png`);
+
+                var buf = <any>await img.getBufferAsync("image/jpeg");
+                break;
+            }
+        }
+
+        if (!buf) return null;
+        let { h, w } = res.images.pages[i];
+        let doc = new Pdf.Document({ height: h, width: w, font: null });
+        doc.image(new Pdf.Image(buf.data), { align: "center" });
+
+        return doc.asBuffer();
+    });
+
+    let meta = { author: "isla", creator: "isla", subject: "Doujin", title: folder };
+    let pdf = new Pdf.Document({ properties: meta, font: null });
+    pdf.pipe(destination, { end: true });
+    let resolve = await Promise.all(promises);
+
+    pdf.text(INFORMATION_FILE);
+    resolve.forEach((buffer, index) => {
+        if (!buffer) return;
+        let image = new Pdf.ExternalDocument(buffer);
+        pdf.addPagesOf(image);
+    });
+
+    pdf.end();
+    // const finalFile = await zip.generateAsync({ type: "arraybuffer" });
+
+    return;
+}
+
+declare global {
+    interface SuperAgentStatic {
+        proxy(host: string);
+    }
 }
--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
 //Library
 import express, { Request, NextFunction, Response } from "express";
-import * as fs from "fs";
+// import * as fs from "fs";
+import axios from "axios";
 import { join as pathJoin } from "path";
 import { config } from "dotenv";
 
 //Module
-import {} from "./lib/nhentai";
+import { search } from "./lib/nhentai";
 
 //Constants
 config();
@@ -39,9 +40,34 @@ app.get("/search", (req, res) => {
     res.render("search");
 });
 
+app.get("/download", async (req, res) => {
+    let { id } = req.query;
+    if (!id) return res.status(400).render("error", { title: "Error 400", error: "Error 400", message: "Missing required parameter 'id'", description: "Bad Request" });
+    // console.log(id);
+    let doujin = await search(<string>id);
+    // console.log(doujin);
+    if (!doujin?.id) return res.status(404).render("error", { error: "Error 404", title: "Error 404", message: "Doujin Not Found", description: "Doujin Not Found" });
+
+    let thumb64 = await axios.get(`https://i2.nhentai.net/galleries/${doujin.media_id}/1.${doujin.images.pages[0].t === "j" ? "jpg" : "png"}`, { responseType: "arraybuffer" });
+    res.render("download", {
+        id: doujin.id.toString(),
+        title: doujin.title.pretty,
+        parodies: doujin.parodies,
+        artists: doujin.artists,
+        categories: doujin.categories,
+        characters: doujin.characters,
+        languages: doujin.languages,
+        groups: doujin.groups,
+        tags: doujin.tags,
+        pageCount: doujin.num_pages,
+        thumb64: `data:image/jpeg;base64,${Buffer.from(thumb64.data).toString("base64")}`,
+        // thumb64: `https://i2.nhentai.net/galleries/${doujin.media_id}/cover.${doujin.images.cover.t === "j" ? "jpg" : "png"}`,
+    });
+});
+
 //Fallback
 app.use((req, res) => {
-    res.status(404).render("error", { error: "Error 404", title: "Error 404", message: "Page Not Found" });
+    res.status(404).render("error", { error: "Error 404", title: "Error 404", message: "Page Not Found", description: "Page Not Found" });
 });
 
 //Run
