5c487affc720e2de4f8a3c61a365511d91c522f6
ISLA <65892564+mangadi3859@users.noreply.github.com>
2022-01-20 09:23:52 +0800

    update api.ts

--- routes/api.ts
+++ routes/api.ts
@@ -75,9 +75,9 @@ async function checkRelatedPayload(req: Request, res: Response, next: NextFuncti
 }
 
 async function checkQueryPayload(req: Request, res: Response, next: NextFunction): Promise<any> {
-    const { query, sort, pages } = req.query;
+    const { query, sort, page } = req.query;
     if (!query) return res.status(400).json(ErrorCode.BAD_REQUEST);
-    let doujin = await searchByTerm(<string>query, { sort: <Utils.Popular>sort ?? "", pages: parseInt(<string>pages) || null}).catch(console.error);
+    let doujin = await searchByTerm(<string>query, { sort: <Utils.Popular>sort ?? "", page: parseInt(<string>page) || null}).catch(console.error);
     if (!doujin) return res.status(404).json(ErrorCode.NOT_FOUND);
 
     req["queryResult"] = doujin;
